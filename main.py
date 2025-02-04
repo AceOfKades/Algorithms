617a6acd309b758da24ea9d5ed9de807ce6fb0dd
AceOfKades <knelson17@cub.uca.edu>
2025-02-03 20:01:46 -0600

    added lists to hold encrypted and signed messages in main.py
    added encryptedMessages and signedMessages to main.py to hold strings or tuples or whatever we need them to hold, plus updated menus to list their contents and verify that the selection is a valid choice.

--- main.py
+++ main.py
@@ -12,8 +12,11 @@ def main():
     inputText = "Enter your choice: "
     s = "\n\n" #extra space between menus
     
-    keys = functions.genKey() #keys[0] = e, keys[1] = d, keys[2] = n
+    encryptedMessages = []
+    signedMessages = []
     
+    
+    keys = functions.genKey() #keys[0] = e, keys[1] = d, keys[2] = n
     print("RSA keys have been generated.")
     
     while True:
@@ -32,26 +35,40 @@ def main():
                 userInput = input(inputText)
                 
                 if (userInput == "1"): #send encrypted message
-                    messageEncrpyt = input("Enter a message: ")
+                    messageEncrypt = input("Enter a message: ")
+
                     #encrypt message here
+                    
                     #send message here
+                    encryptedMessages.append(messageEncrypt)
+                    
                     print("Message encrypted and sent." + s)
                 elif (userInput == "2"): #Authenticate a digital signature
-                    #if there are messages:
-                        #while True:
-                            #present list of messages availble
-                            #if selection is valid:
+                    if(len(signedMessages) > 0):
+                        while True:
+                            for i, j in zip(range(len(signedMessages)), signedMessages): #present list of messages availble
+                                print(f"{i+1}. {j}")
+                                
+                            userInput = input(inputText)
+                            
+                            try:
+                                int(userInput)
+                            except:
+                                print(error)
+                                continue
+                            
+                            if (int(userInput)-1 in range(len(signedMessages))):
                                 #authenticate signature
-                                #if authentic:
-                                    #print("Signature is valid.")
-                                #else:
-                                    #print("Signature is invalid.)
-                                #break
-                            #else:
-                                #print(error)
-                    #elif there are not messages:
-                        #print("There are no signature to authenticate.)
-                    print("code is happening here..." + s) #placeholder to indicated this optioon was selected
+                                authentic = True #put authentication here
+                                if authentic:
+                                    print("Signature is valid."+ s)
+                                else:
+                                    print("Signature is invalid."+ s)
+                                break
+                            else:
+                                print(error)
+                    else:
+                        print("There are no signatures to authenticate." + s)
                 elif (userInput == "3"): # exit
                     break
                 else:
@@ -70,21 +87,40 @@ def main():
                 userInput = input(inputText)
                 
                 if (userInput == "1"): # decrypt received messages
-                    #while True:
-                        print("The following messages are available: ")
-                        #present numbered list of messages of (length = n) here
-                        #userInput = input(inputText)
-                        #if valid choice:
-                            #decrypt message
-                            #print("Decrypted message: " + decrypted message)
-                            #break
-                        #else:
-                            #print(error)
-                            #continue
-                        print(s)
+                    if (len(encryptedMessages) > 0): #if there are messages to decrypt
+                        while True:
+                            #present numbered list of messages of (length = n) here
+                            print("The following messages are available: ")
+                            
+                            for i, j in zip(range(len(encryptedMessages)), encryptedMessages):
+                                print(f"{i+1}. {j}")
+                            userInput = input(inputText)
+                            
+                            try:
+                                int(userInput)
+                            except:
+                                print(error)
+                                continue
+                            
+                            if(int(userInput)-1 in range(len(encryptedMessages))):
+                                #decrypt message
+                                print("Decrypted message: ")
+                                break
+                            else:
+                                print(error)
+                                continue
+                            print(s)
+                    else:   #if there are no messages to decrypt
+                        print("No messages available to decrypt." + s)
+                        
                 elif (userInput == "2"): #digital signature
-                    signedMessage = input("Enter a message: ")
+                    signMessage = input("Enter a message: ")
+                    
                     #sign message
+                    
+                    #send message
+                    signedMessages.append(signMessage)
+                    
                     print("Message signed and sent." + s)
                     
                 elif (userInput == "3"): #Show keys
